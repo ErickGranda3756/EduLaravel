053f1dafe10acb993cf2d739802264296328d631
Erick Granda <75343631+ErickGranda3756@users.noreply.github.com>
2024-09-21 18:22:11 -0500

    Generar registros de prueba con factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +19,20 @@ class DatabaseSeeder extends Seeder
     {
         /* LLamar al seeder */
         $this->call(UserSeeder::class);
+        //eliminar carpeta articles
+        Storage::deleteDirectory('articles');
+
+        //eliminar la carpeta categories
+        Storage::deleteDirectory('categories');
+
+            //crear carpeta articles
+        Storage::makeDirectory('articles');
+
+            //Crea la carpeta categories
+        Storage::makeDirectory('categories');
+        /* Factories */
+        Category::factory(8)->create();
+        Article::factory(20)->create();
+        Comment::factory(20)->create();
     }
 }
