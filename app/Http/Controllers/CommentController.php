a3aa37f036f65422645b3d6877c6383185ca7b0c
Erick Granda <75343631+ErickGranda3756@users.noreply.github.com>
2024-10-11 13:01:04 -0500

    listar comentarios e eliminacion

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -20,9 +20,10 @@ class CommentController extends Controller
         $comments = DB::table("comments")
             ->join("articles", "comments.article_id", "=", "articles.id")
             ->join("users", "comments.user_id", "=", "users.id")
-            ->select("comments.value", "comments.description", "articles.title", "users.full_name")
+            ->select("comments.id","comments.value", "comments.description", "articles.title", "users.full_name")
             ->where("articles.user_id", "=", Auth::user()->id)
-            ->orderBy("articles.id", "desc");
+            ->orderBy("articles.id", "desc")
+            ->get();
         return view("admin.comments.index", compact("comments"));
     }
 
