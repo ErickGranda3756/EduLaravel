e3f9f9dc6995b3a72d9ede6d882ed2502d39369e
Erick Granda <75343631+ErickGranda3756@users.noreply.github.com>
2024-10-12 18:52:25 -0500

    Asignar permisos desde el administrador

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,11 +7,7 @@
 @endsection
 
 @section('content')
-@if(session("success-update"))
-    <div class="alert alert-info">
-        {{session("success-update")}}
-    </div>
-@elseif(session("success-delete"))
+@if(session("success-delete"))
     <div class="alert alert-info">
         {{session("success-delete")}}
     </div>
